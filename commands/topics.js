443309c8e21695d0680243d08399ce8b67c37520
Tom Crayford <tcrayford@googlemail.com>
2015-08-25 14:47:24 +0100

    create a topic

--- commands/topics.js
+++ commands/topics.js
@@ -1,4 +1,30 @@
 'use strict';
+
+let zookeeper = require("node-zookeeper-client");
+let cli = require('heroku-cli-util');
+let co = require('co');
+
+function* createTopic (context, heroku) {
+    let config = yield heroku.apps(context.app).configVars().info();
+    let zookeeperURL = config['HEROKU_KAFKA_ZOOKEEPER_URL'].replace(/zk:\/\//g,'');
+    let topicName = context.flags.topic;
+
+    let data = {version:1, partitions: {"0":[1]}};
+
+    let client = zookeeper.createClient(zookeeperURL);
+    client.once('connected', function () {
+      client.create("/brokers/topics/" + topicName, new Buffer(JSON.stringify(data)), function (error, path) {
+        if (error) {
+          cli.error(error);
+        } else {
+          console.info("created topic ", topicName);
+        }
+        client.close()
+      });
+    });
+    client.connect();
+}
+
 module.exports = {
   topic: 'kafka',
   command: 'create-topic',
@@ -9,7 +35,5 @@ module.exports = {
   flags: [
     {name: 'topic', char: 't', description: 'topic name to create', hasValue: true, optional: false}
   ],
-  run: function (context) {
-    console.log('hello world' + context.flags.topic);
-  }
+  run: cli.command(co.wrap(createTopic)),
 }
