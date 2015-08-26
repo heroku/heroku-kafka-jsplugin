2b9a3085ce7f10db7be94e87fd6589882f6aa948
Tom Crayford <tcrayford@googlemail.com>
2015-08-26 16:23:50 +0100

    move kafka resource into a new file

--- commands/info.js
+++ commands/info.js
@@ -2,8 +2,7 @@
 
 let cli = require('heroku-cli-util');
 let co = require('co');
-
-let VERSION = "v0";
+let HerokuKafkaResource = require('./resource.js').HerokuKafkaResource;
 
 function addonInfoWithName(addonInfo, name) {
   return addonInfo.info.filter(function (field) {
@@ -24,40 +23,6 @@ function formatInfo(addonInfo) {
   });
   return out.join("\n");
 }
-
-function HerokuKafkaResource(heroku, env, context) {
-  this.heroku = heroku;
-  this.env = env;
-  this.context = context;
-  this.app = context.app;
-}
-
-HerokuKafkaResource.prototype.info = function* () {
-  var addon = yield this.addon();
-  var response = yield this.heroku.request({
-    host: this.host(),
-    path: `/client/kafka/${VERSION}/clusters/${addon.name}`,
-    auth: `${this.context.auth.username}:${this.context.auth.password}`,
-    headers: {
-      'Accept': 'application/json'
-    }
-  });
-  return response;
-};
-
-HerokuKafkaResource.prototype.host = function () {
-  if (this.env.SHOGUN) {
-    return "shogun-" + this.env.SHOGUN + ".herokuapp.com";
-  } else {
-    return "postgres-api.heroku.com";
-  }
-};
-
-HerokuKafkaResource.prototype.addon = function* () {
-  let availableAddons = yield this.heroku.apps(this.app).addons().listByApp();
-  let kafkaAddon = availableAddons.filter(function (addon) { return addon.addon_service.name === 'heroku-kafka'; });
-  return kafkaAddon[0];
-};
 
 
 function* kafkaInfo (context, heroku) {
---
+++ commands/resource.js
@@ -0,0 +1,41 @@
+'use strict';
+
+let VERSION = "v0";
+
+function HerokuKafkaResource(heroku, env, context) {
+  this.heroku = heroku;
+  this.env = env;
+  this.context = context;
+  this.app = context.app;
+}
+
+HerokuKafkaResource.prototype.info = function* () {
+  var addon = yield this.addon();
+  var response = yield this.heroku.request({
+    host: this.host(),
+    path: `/client/kafka/${VERSION}/clusters/${addon.name}`,
+    auth: `${this.context.auth.username}:${this.context.auth.password}`,
+    headers: {
+      'Accept': 'application/json'
+    }
+  });
+  return response;
+};
+
+HerokuKafkaResource.prototype.host = function () {
+  if (this.env.SHOGUN) {
+    return "shogun-" + this.env.SHOGUN + ".herokuapp.com";
+  } else {
+    return "postgres-api.heroku.com";
+  }
+};
+
+HerokuKafkaResource.prototype.addon = function* () {
+  let availableAddons = yield this.heroku.apps(this.app).addons().listByApp();
+  let kafkaAddon = availableAddons.filter(function (addon) { return addon.addon_service.name === 'heroku-kafka'; });
+  return kafkaAddon[0];
+};
+
+module.exports = {
+  HerokuKafkaResource: HerokuKafkaResource
+};
