b547dfb7993468a22d3a9b1980407827b05e31d2
Tom Crayford <tcrayford@googlemail.com>
2015-08-27 13:38:40 +0100

    nicer kafka:wait output
    add a spinner, and the number of seconds it took for kafka to become
    available once it's up.

--- commands/wait.js
+++ commands/wait.js
@@ -1,8 +1,7 @@
-'use strict';
-
-let cli = require('heroku-cli-util');
-let co = require('co');
-let HerokuKafkaResource = require('./resource.js').HerokuKafkaResource;
+var cli = require('heroku-cli-util');
+var co = require('co');
+var HerokuKafkaResource = require('./resource.js').HerokuKafkaResource;
+var Spinner = require('node-spinner');
 
 function addonInfoWithName(addonInfo, name) {
   return addonInfo.info.filter(function (field) {
@@ -12,15 +11,24 @@ function addonInfoWithName(addonInfo, name) {
 
 function* kafkaWait (context, heroku) {
   var finished = false;
+  var s = Spinner();
+  var startTime = new Date().getTime() / 1000;
+  var ticks = 0;
   while (!finished) {
     var info = yield new HerokuKafkaResource(heroku, process.env, context).info();
     var nodeStatuses = addonInfoWithName(info, 'Status').values;
     var availableNodes = nodeStatuses.filter(function (nodeStatus) { return nodeStatus.endsWith('available'); });
     if (nodeStatuses.length === availableNodes.length) {
-      console.log("finished");
       finished = true;
+      if (ticks === 0) {
+        process.stdout.write("all kafka nodes already available, no waiting required");
+      } else {
+        var finishedTime = new Date().getTime() / 1000;
+        process.stdout.write("\rfinished in " +  (finishedTime - startTime).toFixed(1) + " seconds");
+      }
     } else {
-      console.log("waiting for nodes to be available");
+      process.stdout.write("\r \033[36mwaiting\033[m " + s.next());
+      ticks += 1;
     }
   }
 }
