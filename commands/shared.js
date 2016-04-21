ab6e1c71dfc4d8d7be724eb3b1ce891d2e8287ad
Tom Crayford <tcrayford@googlemail.com>
2016-04-21 14:34:38 +0100

    remove cruft
    this is leftover from when topic creation happened in this plugin

--- commands/shared.js
+++ commands/shared.js
@@ -2,45 +2,6 @@
 
 const _ = require('underscore');
 
-const validTopicRegex = /^[a-zA-Z0-9\\._\\-]+$/;
-const adminTopics = ["__consumer_offsets"];
-
-function collidesWithExistingTopics(topicName, existingTopics) {
-  var replaced = topicName.replace(/\./g, '_');
-  for (var i = 0; i < existingTopics.length; i++) {
-    if (replaced === existingTopics[i].replace(/\./g, '_')) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function checkValidTopicName(topicName, existingTopics) {
-  if (topicName.length <= 0) {
-    return {invalid: true, message: "length must be greater than 0"};
-  } else if (topicName === "." || topicName === "..") {
-    return {invalid: true, message: "topic name must not be '.' or '..'"};
-  } else if (topicName.length > 255) {
-    return {invalid: true, message: "topic names can't be longer than 255 chars"};
-  } else if (!topicName.match(validTopicRegex)) {
-    return {invalid: true, message: "topic name contains characters other than ASCII alphanumerics, '.', '_', and '-'"};
-  } else if (collidesWithExistingTopics(topicName, existingTopics)) {
-    return {invalid: true, message: "topic name collids with existing topics"};
-  } else {
-    return {invalid: false};
-  }
-}
-
-function checkValidTopicNameForDeletion(topicName, existingTopics) {
-  if (existingTopics.indexOf(topicName) === -1) {
-    return {invalid: true, message: "topic must be an existing topic"};
-  } else if (adminTopics.indexOf(topicName) !== -1) {
-    return {invalid: true, message: "topic cannot be an admin topic"};
-  } else {
-    return {invalid: false};
-  }
-}
-
 function clusterConfig(addon, config) {
   if (!addon) {
     return null;
@@ -66,7 +27,5 @@ function clusterConfig(addon, config) {
 }
 
 module.exports = {
-  checkValidTopicName : checkValidTopicName,
-  checkValidTopicNameForDeletion: checkValidTopicNameForDeletion,
   clusterConfig: clusterConfig
 };
