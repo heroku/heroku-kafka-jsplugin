130b90a36e6068de38940ce2bed5a69b01de2a76
Maciek Sakrejda <maciek@heroku.com>
2016-06-29 15:07:59 -0700

    Clean up docs
     * Remove obsolete 'in milliseconds' from relevant options
     * Change naming to be more consistent with style guidelines https://devcenter.heroku.com/articles/cli-style-guide?preview=1

--- commands/configure_topic.js
+++ commands/configure_topic.js
@@ -1,9 +1,9 @@
 'use strict';
 
 let FLAGS = [
-  {name: 'retention-time', description: 'The length of time in milliseconds messages in the topic should be retained for.',  hasValue: true,  optional: true},
-  {name: 'compaction',     description: 'Enables compaction on the topic if passed',                                         hasValue: false, optional: true},
-  {name: 'no-compaction',  description: 'Disables compaction on the topic if passed',                                        hasValue: false, optional: true}
+  {name: 'retention-time', description: 'length of time messages in the topic should be retained for',  hasValue: true,  optional: true},
+  {name: 'compaction',     description: 'enables compaction on the topic if passed',                    hasValue: false, optional: true},
+  {name: 'no-compaction',  description: 'disables compaction on the topic if passed',                   hasValue: false, optional: true}
 ];
 let DOT_WAITING_TIME = 200;
 
