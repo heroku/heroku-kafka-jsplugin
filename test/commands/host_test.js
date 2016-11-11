4fcaed542cae093afc212d41e75a95ab674a4b33
Tom Crayford <tcrayford@googlemail.com>
2016-11-11 18:59:50 +0000

    fix standard

--- test/commands/host_test.js
+++ test/commands/host_test.js
@@ -4,8 +4,6 @@ const expect = require('chai').expect
 const mocha = require('mocha')
 const describe = mocha.describe
 const it = mocha.it
-const beforeEach = mocha.beforeEach
-const afterEach = mocha.beforeEach
 
 const host = require('../../lib/host.js')
 
