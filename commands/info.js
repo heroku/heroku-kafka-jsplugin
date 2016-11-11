442446586f0ec8abac424cabd793877948f09e48
Tom Crayford <tcrayford@googlemail.com>
2016-11-11 12:55:08 +0000

    use data endpoint for kafka:info

--- commands/info.js
+++ commands/info.js
@@ -10,16 +10,53 @@ function configVarsFromName (attachments, name) {
     .sort((name) => name !== 'KAFKA_URL')
 }
 
+function format_info (cluster) {
+  let lines = [
+    {name: 'Name', values: [cluster.addon.name]},
+    {name: 'Plan', values: [cluster.addon.plan.name]},
+    {name: 'Status', values: [cluster.cluster.state.message]},
+    {name: 'Version', values: [cluster.cluster.version]},
+    {name: 'Created', values: [cluster.cluster.created_at]},
+  ];
+
+  if (cluster.cluster.robot.is_robot) {
+    lines.push({name: 'Robot', values: ["True"]});
+    lines.push({name: 'Robot TTL', values: [cluster.cluster.robot.robot_ttl]});
+  }
+
+  //TODO: pluralize topics correctly
+  lines.push({name: 'Topics', values: [`${cluster.cluster.topics.length} topics, see heroku kafka:topics`]});
+
+  //TODO: format with commas
+  lines.push({name: 'Messages', values: [`${cluster.cluster.messages_in_per_sec} messages/s`]});
+
+  //TODO: format as human readable sizes
+  lines.push({name: 'Traffic', values: [`${cluster.cluster.bytes_in_per_sec} bytes/s in / ${cluster.cluster.bytes_out_per_sec} bytes/s out`]});
+
+  //TODO: format as human readable size
+  if (cluster.cluster.data_size && cluster.cluster.limits.limit_bytes) {
+    let size = cluster.cluster.data_size;
+    let limit = cluster.cluster.limits.limit_bytes;
+    let percentage = ((size / limit) * 100.0).toFixed(2)
+    lines.push({name: 'Data Size', values: [`${cluster.cluster.data_size.size} / ${cluster.cluster.limits.limit_bytes} (${percentage})`]})
+  }
+
+  lines.push({name: 'Add-on', values: [cli.color.addon(cluster.addon.name)]});
+
+  return lines;
+}
+
 function displayCluster (cluster) {
   cli.styledHeader(cluster.configVars.map(c => cli.color.configVar(c)).join(', '))
-  cluster.cluster.info.push({name: 'Add-on', values: [cli.color.addon(cluster.addon.name)]})
-  let info = cluster.cluster.info.reduce((info, i) => {
+
+  let cluster_info = format_info(cluster);
+  let info = cluster_info.reduce((info, i) => {
     if (i.values.length > 0) {
       info[i.name] = i.values.join(', ')
     }
     return info
   }, {})
-  let keys = cluster.cluster.info.map(i => i.name)
+  let keys = cluster_info.map(i => i.name)
   cli.styledObject(info, keys)
   cli.log()
 }
