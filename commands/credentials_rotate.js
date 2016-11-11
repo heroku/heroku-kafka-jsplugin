a1ba47ee97231803ac752d70392559940a8c4ad0
Tom Crayford <tcrayford@googlemail.com>
2016-11-11 11:12:22 +0000

    cli command for rotating client credentials

---
+++ commands/credentials_rotate.js
@@ -0,0 +1,45 @@
+'use strict'
+
+let cli = require('heroku-cli-util')
+let co = require('co')
+let withCluster = require('../lib/clusters').withCluster
+let request = require('../lib/clusters').request
+
+const VERSION = 'v0'
+
+function * credentialsRotate (context, heroku) {
+  yield withCluster(heroku, context.app, context.args.CLUSTER, function * (addon) {
+    let response = yield request(heroku, {
+      method: 'POST',
+      path: `/client/kafka/${VERSION}/clusters/${addon.name}/rotate-credentials`
+    })
+
+    cli.log(response.message)
+  })
+}
+
+module.exports = {
+  topic: 'kafka',
+  command: 'credentials',
+  description: 'triggers credential rotation',
+  help: `
+    Rotates client certificates
+
+    Examples:
+
+    $ heroku kafka:credentials --reset
+    $ heroku kafka:credentials KAFKA_RED_URL --reset
+`,
+  needsApp: true,
+  needsAuth: true,
+  args: [
+    { name: 'CLUSTER', optional: true }
+  ],
+  flags: [
+    { name: 'reset',
+      description: 'reset credentials',
+      hasValue: false,
+      required: true }
+  ],
+  run: cli.command(co.wrap(credentialsRotate))
+}
