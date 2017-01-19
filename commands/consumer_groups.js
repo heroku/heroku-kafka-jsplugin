9bda01e471e0af30f1ec137e2826150cad87e6f2
Tim Carey-Smith <tim@spork.in>
2017-01-18 16:45:44 -0800

    Add support for managing consumer groups (#130)

---
+++ commands/consumer_groups.js
@@ -0,0 +1,57 @@
+'use strict'
+
+let cli = require('heroku-cli-util')
+let co = require('co')
+let withCluster = require('../lib/clusters').withCluster
+let request = require('../lib/clusters').request
+
+const VERSION = 'v0'
+
+function * listConsumerGroups (context, heroku) {
+  yield withCluster(heroku, context.app, context.args.CLUSTER, function * (addon) {
+    let consumerGroups = yield request(heroku, {
+      path: `/data/kafka/${VERSION}/clusters/${addon.id}/consumer_groups`
+    })
+    cli.styledHeader('Kafka Consumer Groups on ' + (context.args.CLUSTER || 'HEROKU_KAFKA'))
+    let consumerGroupData = consumerGroups.consumer_groups.map((g) => {
+      return {
+        name: g.name
+      }
+    })
+    cli.log()
+    if (consumerGroupData.length === 0) {
+      cli.log('No consumer groups found on this Kafka cluster.')
+      cli.log('Use heroku kafka:consumer-groups:create to create a consumer group.')
+    } else {
+      cli.table(consumerGroupData,
+        {
+          columns: [
+            {key: 'name', label: 'Name'}
+          ]
+        }
+      )
+    }
+  })
+}
+
+module.exports = {
+  hidden: true,
+  topic: 'kafka',
+  command: 'consumer-groups',
+  description: 'lists available Kafka consumer groups',
+  help: `
+    Lists available Kafka consumer groups.
+
+    Examples:
+
+    $ heroku kafka:consumer-groups
+    $ heroku kafka:consumer-groups kafka-aerodynamic-32763
+`,
+
+  args: [
+    { name: 'CLUSTER', optional: true }
+  ],
+  needsApp: true,
+  needsAuth: true,
+  run: cli.command({preauth: true}, co.wrap(listConsumerGroups))
+}
---
+++ commands/consumer_groups_create.js
@@ -0,0 +1,53 @@
+'use strict'
+
+let cli = require('heroku-cli-util')
+let co = require('co')
+let withCluster = require('../lib/clusters').withCluster
+let request = require('../lib/clusters').request
+
+const VERSION = 'v0'
+
+function * createConsumerGroup (context, heroku) {
+  yield withCluster(heroku, context.app, context.args.CLUSTER, function * (addon) {
+    let msg = `Creating consumer group ${context.args.CONSUMER_GROUP}`
+    if (context.args.CLUSTER) {
+      msg += ` on ${context.args.CLUSTER}`
+    }
+
+    yield cli.action(msg, co(function * () {
+      return yield request(heroku, {
+        method: 'POST',
+        body: {
+          consumer_group: {
+            name: context.args.CONSUMER_GROUP
+          }
+        },
+        path: `/data/kafka/${VERSION}/clusters/${addon.id}/consumer_groups`
+      })
+    }))
+
+    cli.log(`Use \`heroku kafka:consumer-groups:info ${context.args.CONSUMER_GROUP}\` to monitor your consumer group.`)
+  })
+}
+
+module.exports = {
+  hidden: true,
+  topic: 'kafka',
+  command: 'consumer-groups:create',
+  description: 'creates a consumer group in Kafka',
+  help: `
+    Creates a consumer group in Kafka.
+
+    Examples:
+
+  $ heroku kafka:consumer-groups:create word-counters
+  $ heroku kafka:consumer-groups:create word-counters kafka-aerodynamic-32763
+  `,
+  needsApp: true,
+  needsAuth: true,
+  args: [
+    { name: 'CONSUMER_GROUP' },
+    { name: 'CLUSTER', optional: true }
+  ],
+  run: cli.command({preauth: true}, co.wrap(createConsumerGroup))
+}
