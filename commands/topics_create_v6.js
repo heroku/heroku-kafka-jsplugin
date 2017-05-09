a6f11a86292b26646f948440932fca29a4409a95
Philipe Navarro <RasPhilCo@users.noreply.github.com>
2017-05-09 12:48:56 -0700

    Remove flow

--- commands/topics_create_v6.js
+++ commands/topics_create_v6.js
@@ -1,32 +1,23 @@
 'use strict';
 
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
+const Command = require('cli-engine-command').default
+const AppFlag = require('cli-engine-command/lib/flags/app').AppFlag
+const BooleanFlag = require('cli-engine-command/lib/flags/boolean').default
+const StringFlag = require('cli-engine-command/lib/flags/string').default
 
-var _cliEngineCommand = require('cli-engine-command');
+const DurationFlag = require('../lib/flags/duration')
 
-var _cliEngineCommand2 = _interopRequireDefault(_cliEngineCommand);
-
-var _app = require('cli-engine-command/lib/flags/app');
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-let appParser = function (app) {
-  return new Promise(resolve => resolve(`${app}-test`));
-};
-
-class _class extends _cliEngineCommand2.default {
-
+class TopicCreate extends Command {
   async run(args) {
-    this.out.log(`running create... for ${this.flags.app}`);
+    this.out.log(`running create with retention-time: ${this.flags['retention-time']} ms`)
   }
 }
-exports.default = _class;
-_class.topic = 'kafka';
-_class.command = '_topics:create';
-_class.description = 'creates a topic in Kafka';
-_class.help = `
+
+exports.default = TopicCreate
+TopicCreate.topic = 'kafka'
+TopicCreate.command = '_topics:create';
+TopicCreate.description = 'creates a topic in Kafka'
+TopicCreate.help = `
     Creates a topic in Kafka. Defaults to time-based retention according to plan
     minimum if not explicitly specified.
 
@@ -36,11 +27,14 @@ _class.help = `
   $ heroku kafka:topics:create page-visits kafka-shiny-2345 --partitions 100 --replication-factor 3 --retention-time 10d
   $ heroku kafka:topics:create page-visits kafka-shiny-2345 --partitions 100 --compaction
   `;
-_class.args = [{ name: 'TOPIC' }, { name: 'CLUSTER', optional: true }];
-_class.flags = {
-  app: (0, _app.AppFlag)({ required: true, parse: appParser }),
-  partitions: (0, _cliEngineCommand.StringFlag)({ description: 'number of partitions to give the topic' }),
-  'replication-factor': (0, _cliEngineCommand.StringFlag)({ description: 'number of replicas the topic should be created across' }),
-  'retention-time': (0, _cliEngineCommand.StringFlag)({ description: 'length of time messages in the topic should be retained (at least 24h)' }),
-  compaction: (0, _cliEngineCommand.BooleanFlag)({ description: 'whether to use compaction for this topic' })
-};
+TopicCreate.args = [
+  {name: 'TOPIC'},
+  {name: 'CLUSTER', optional: true}
+]
+TopicCreate.flags = {
+  app: AppFlag({ required: true}),
+  partitions: StringFlag({ description: 'number of partitions to give the topic' }),
+  'replication-factor': StringFlag({ description: 'number of replicas the topic should be created across' }),
+  'retention-time': DurationFlag({ description: 'length of time messages in the topic should be retained (at least 24h)' }),
+  compaction: BooleanFlag({ description: 'whether to use compaction for this topic' })
+}
